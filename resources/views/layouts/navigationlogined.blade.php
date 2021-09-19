66ea71eb42dbb4c62ca43c0246eac375a5d4c59f
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-09-19 15:53:29 +0900

    logoをAtteに変更

--- resources/views/layouts/navigationlogined.blade.php
+++ resources/views/layouts/navigationlogined.blade.php
@@ -3,11 +3,9 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- Atte -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    <p>Atte</p>
                 </div>
 
                 <!-- Navigation Links -->
