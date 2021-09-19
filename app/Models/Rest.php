604f179e842b2605c3bb3e88e1c84afac19f9f1b
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-09-19 17:52:59 +0900

    Restモデル修正

--- app/Models/Rest.php
+++ app/Models/Rest.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Rest extends Model
 {
     use HasFactory;
+
+    public function Attendance(){
+        return $this->belongsTo('App\Models\Attendance');
+    }
+    protected $fillable = ['start_time', 'end_time'];
 }
