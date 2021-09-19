c6b3e2e59a8a133b9650d4ef3c73d50b25f2ccfc
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-09-19 17:18:46 +0900

    AttendanceController 作成

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //
+    public function start(Request $request){
+        $this->validate($request, Attendance::rules);
+        $form = $request->all();
+        Attendance::create($form);
+        return redirect('');
+    }
+    public function end(Request $request){
+        $attendance = attendance::find($request->id);
+        $this->validate($request, Attendance::rules);
+        $form = $request->all();
+        unset($form["_token"]);
+        Attendance::update($form);
+        return redirect('');
+    }
 }
