db6e0355bebc2e1fe5cad34be513fd9c8e8a2909
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-09-19 16:44:52 +0900

    web copy.php バックアップ

--- routes/web copy.php
+++ routes/web copy.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserContoroller;
-use App\Http\Controllers\TimeContoroller;
-use App\Http\Controllers\SessionContoroller;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,31 +16,23 @@ use App\Http\Controllers\SessionContoroller;
 |
 */
 
-Route::get('/home',[TimeController::class,'index'])->middleware('auth');
-Route::post('/start',[TimeController::class,'start']);
-Route::post('/close',[TimeController::class,'close']);
-Route::post('/breakstart',[TimeController::class,'breakstart']);
-Route::post('/breakend',[TimeController::class,'breakend']);
-Route::get('/attendance',[TimeController::class,'attendance'])->middleware('auth');
-
-Route::get('/register',[AtteUserController::class,'register']);
-Route::post('/register',[AtteUserController::class,'regist']);
-Route::get('/login',[AtteUserController::class,'login']);
-Route::post('/loggin',[AtteUserController::class,'loggin']);
-Route::post('/logout',[AtteUserController::class,'logout']);
-
 Route::get('/session', [SessionController::class, 'getSes']);
 Route::post('/session', [SessionController::class, 'postSes']);
 
-Route::get('/auth', [AuthorController::class,'check']);
-Route::post('/auth', [AuthorController::class,'checkUser']);
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware(['auth'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/attendance', function () {
+    return view('attendance');
+})->middleware(['auth'])->name('attendance');
+
 require __DIR__.'/auth.php';
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
