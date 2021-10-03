19ed60308ab097bd726077b9d98c48458276230d
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-03 12:10:32 +0900

    勤務開始打刻時に出勤中でないことを確認。

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -9,13 +9,56 @@ use Illuminate\Http\Request;
 class AttendanceController extends Controller
 {
     public function start(Request $request){
-        // $this->validate($request, Attendance::$rules);
+
+        // 出勤中でないことを確認。
+        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($atte['start_time']);
+        $end_time = strtotime($atte['end_time']);
+        // dd($atte);
+        // dd(start_time);
+        // dd($end_time);
+        $notatte = $end_time-$start_time;
+        // var_dump($notatte);
+        // dd($notatte);
+        // $s = date('Y/m/d H:i:s', $start_time);
+        // dd($s);
+        // $w = date('Y/m/d H:i:s', $end_time);
+        // dd($w);
+        // $input = [
+        //     'start_time' => '$start_time',
+        //     'end_time' => '$end_time'
+        // ];
+        // dd($input);
+        // $filtered = $atte->only(['start_time', 'end_time']);
+        // $filtered->toArray();
+        // dd($filtered);
+        // $validate_rule = [
+        //     // 'notatte' => 'different:0',
+        //     'end_time' => 'date|after:start_time',
+        // ];
+        // dd($validate_rule);
+
+        if($notatte == 0){
+            return redirect('');
+        }else{
+            // $this->validate($request, Attendance::$rules);
         $form = $request->all();
         $form['user_id'] = Auth::user()->id;
         Attendance::create($form);
         return redirect('');
+        }
     }
     public function end(Request $request){
+
+        // 出勤中であることを確認。
+        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($atte['start_time']);
+        $end_time = strtotime($atte['end_time']);
+        $notatte = $end_time-$start_time;
+
+        // 休憩中でないことを確認。
+
+
         // $this->validate($request, Attendance::rules);
         $form = $request->all();
         $form['end_time'] = now();
