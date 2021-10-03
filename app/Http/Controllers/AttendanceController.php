47026b890fd12553cff47f84348ba7fef5b77e44
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-03 15:04:11 +0900

    一部修正

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -12,15 +12,15 @@ class AttendanceController extends Controller
     public function start(Request $request){
 
         // 出勤中でないことを確認。
-        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
-        $start_time = strtotime($atte['start_time']);
-        $end_time = strtotime($atte['end_time']);
-        // dd($atte);
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($attendance['start_time']);
+        $end_time = strtotime($attendance['end_time']);
+        // dd($attendance);
         // dd(start_time);
         // dd($end_time);
-        $notatte = $end_time-$start_time;
-        // var_dump($notatte);
-        // dd($notatte);
+        $notattendance = $end_time-$start_time;
+        // var_dump($notattendance);
+        // dd($notattendance;
         // $s = date('Y/m/d H:i:s', $start_time);
         // dd($s);
         // $w = date('Y/m/d H:i:s', $end_time);
@@ -34,12 +34,12 @@ class AttendanceController extends Controller
         // $filtered->toArray();
         // dd($filtered);
         // $validate_rule = [
-        //     // 'notatte' => 'different:0',
+        //     // 'notattendance' => 'different:0',
         //     'end_time' => 'date|after:start_time',
         // ];
         // dd($validate_rule);
 
-        if($notatte == 0){
+        if($notattendance == 0){
             return redirect('');
         }else{
             // $this->validate($request, Attendance::$rules);
@@ -52,13 +52,13 @@ class AttendanceController extends Controller
     public function end(Request $request){
 
         // 出勤中であることを確認。
-        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
-        $start_time = strtotime($atte['start_time']);
-        $end_time = strtotime($atte['end_time']);
-        $notatte = $end_time-$start_time;
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($attendance['start_time']);
+        $end_time = strtotime($attendance['end_time']);
+        $notattendance = $end_time-$start_time;
 
         // 休憩中でないことを確認。
-        $rest = Rest::where('attendance_id', $atte['id'])->latest('id')->first();
+        $rest = Rest::where('attendance_id', $attendance['id'])->latest('id')->first();
         if(isset($rest)){
             $rest_start_time = strtotime($rest['start_time']);
             $rest_end_time = strtotime($rest['end_time']);
@@ -66,9 +66,9 @@ class AttendanceController extends Controller
         };
         // dd($rest);
         // dd($notrest);
-        // dd($notatte);
+        // dd($notattendance);
 
-        if($notatte !== 0){
+        if($notattendance !== 0){
             return redirect('');
         }elseif(isset($rest) && $notrest == 0){
             return redirect('');
@@ -79,7 +79,7 @@ class AttendanceController extends Controller
             $form['end_time'] = now();
             unset($form["_token"]);
             // dd($form);
-            $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first()->update($form);
+            $attendance->update($form);
             return redirect('');
         }
     }
