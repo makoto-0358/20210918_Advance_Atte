9a539f9c3e4e9613c40a76333b8148986d809657
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-03 15:48:43 +0900

    form action, method 変更

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <!-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"> -->
                 <div class="p-6 border-t border-gray-200">
-                    <form action="/attendance/start" method="post">
+                    <form action="/attendance" method="get">
                         @csrf
                         <table>
                             <tr><td><button type="submit">勤務開始</button></td></tr>
