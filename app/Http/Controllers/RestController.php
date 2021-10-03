e0d43e87c7885b513087741e4f83ff09525bdbba
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-03 13:56:12 +0900

    休憩開始条件設定

--- app/Http/Controllers/RestController.php
+++ app/Http/Controllers/RestController.php
@@ -12,17 +12,31 @@ class restController extends Controller
     public function start(Request $request){
 
         // 出勤中であることを確認。
-
+        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($atte['start_time']);
+        $end_time = strtotime($atte['end_time']);
+        $notatte = $end_time-$start_time;
 
         // 休憩中でないことを確認。
+        $rest = Rest::where('attendance_id', $atte['id'])->latest('id')->first();
+        if(isset($rest)){
+            $rest_start_time = strtotime($rest['start_time']);
+            $rest_end_time = strtotime($rest['end_time']);
+            $notrest = $rest_end_time-$rest_start_time;
+        };
 
-
-        // $this->validate($request, Rest::$rules);
-        $form = $request->all();
-        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
-        $form['attendance_id'] = $attendance->id;
-        Rest::create($form);
-        return redirect('');
+        if($notatte !== 0){
+            return redirect('');
+        }elseif(isset($rest) && $notrest == 0){
+            return redirect('');
+        }else{
+            // $this->validate($request, Rest::$rules);
+            $form = $request->all();
+            $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+            $form['attendance_id'] = $attendance->id;
+            Rest::create($form);
+            return redirect('');
+        }
     }
     public function end(Request $request){
 
