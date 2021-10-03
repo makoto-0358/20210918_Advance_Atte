ced83f4b40b64e18a0909439a60ce61291c54e75
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-03 14:57:26 +0900

    休憩終了条件設定、その他一部修正

--- app/Http/Controllers/RestController.php
+++ app/Http/Controllers/RestController.php
@@ -12,27 +12,26 @@ class restController extends Controller
     public function start(Request $request){
 
         // 出勤中であることを確認。
-        $atte = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
-        $start_time = strtotime($atte['start_time']);
-        $end_time = strtotime($atte['end_time']);
-        $notatte = $end_time-$start_time;
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
+        $start_time = strtotime($attendance['start_time']);
+        $end_time = strtotime($attendance['end_time']);
+        $notattendance = $end_time-$start_time;
 
         // 休憩中でないことを確認。
-        $rest = Rest::where('attendance_id', $atte['id'])->latest('id')->first();
+        $rest = Rest::where('attendance_id', $attendance['id'])->latest('id')->first();
         if(isset($rest)){
             $rest_start_time = strtotime($rest['start_time']);
             $rest_end_time = strtotime($rest['end_time']);
             $notrest = $rest_end_time-$rest_start_time;
         };
 
-        if($notatte !== 0){
+        if($notattendance !== 0){
             return redirect('');
         }elseif(isset($rest) && $notrest == 0){
             return redirect('');
         }else{
             // $this->validate($request, Rest::$rules);
             $form = $request->all();
-            $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
             $form['attendance_id'] = $attendance->id;
             Rest::create($form);
             return redirect('');
@@ -40,15 +39,33 @@ class restController extends Controller
     }
     public function end(Request $request){
 
-        // 休憩中であることを確認。
-
-
-        // $this->validate($request, Rest::rules);
-        $form = $request->all();
-        $form['end_time'] = now();
-        unset($form["_token"]);
+        // 出勤中であることを確認。
         $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
-        $rest = Rest::where('attendance_id', $attendance->id)->latest('id')->first()->update($form);
-        return redirect('');
+        $start_time = strtotime($attendance['start_time']);
+        $end_time = strtotime($attendance['end_time']);
+        $notattendance = $end_time-$start_time;
+
+        // 休憩中であることを確認。
+        $rest = Rest::where('attendance_id', $attendance['id'])->latest('id')->first();
+        if(isset($rest)){
+            $rest_start_time = strtotime($rest['start_time']);
+            $rest_end_time = strtotime($rest['end_time']);
+            $notrest = $rest_end_time-$rest_start_time;
+        };
+
+        if($notattendance !== 0){
+            return redirect('');
+        }elseif($rest == null){
+            return redirect('');
+        }elseif(isset($rest) && $notrest !== 0){
+            return redirect('');
+        }else{
+            // $this->validate($request, Rest::rules);
+            $form = $request->all();
+            $form['end_time'] = now();
+            unset($form["_token"]);
+            $rest->update($form);
+            return redirect('');
+        }
     }
 }
