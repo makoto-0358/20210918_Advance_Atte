6d777f4792903b1e15c07330c165959501049709
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-16 17:23:52 +0900

    出退勤バリデーション修正

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -14,8 +14,7 @@ class AttendanceController extends Controller
         // 出勤中でないことを確認。
         $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
 
-        if(!empty($attendance) && empty($attendance['end_time'])){
-        }else{
+        if(empty($attendance) || !empty($attendance['end_time'])){
             $form = $request->all();
             $form['user_id'] = Auth::user()->id;
             Attendance::create($form);
@@ -27,13 +26,10 @@ class AttendanceController extends Controller
         // 出勤中であることを確認。
         $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->first();
 
-        if(empty($attendance)){
-        }elseif(!empty($attendance['end_time'])){
-        }else{
+        if(!empty($attendance) || empty($attendance['end_time'])){
             // 休憩中でないことを確認。
             $rest = Rest::where('attendance_id', $attendance['id'])->latest('id')->first();
-            if(!empty($rest) && empty($rest['end_time'])){
-            }else{
+            if(empty($rest) || !empty($rest['end_time'])){
                 $form = $request->all();
                 $form['end_time'] = now();
                 unset($form["_token"]);
