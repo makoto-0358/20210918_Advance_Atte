407f97b8171d86afe0639d42461d8f81a842aa05
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-22 11:14:30 +0900

    form削除

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -7,32 +7,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <!-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"> -->
                 <div class="p-6 border-t border-gray-200">
-                    <form action="/attendance/{date?}" method="get">
-                        @csrf
-                        <table>
+                    <table>
+                        <tr>
+                            <a href="/attendance/{{$beforedate}}">前の日</a>
+                            <td>{{date('Y-m-d', strtotime($date))}}</td>
+                            <a href="/attendance/{{$afterdate}}">次の日</a>
+                        </tr>
+                        <tr>
+                            <th>名前</th>
+                            <th>勤務開始</th>
+                            <th>勤務終了</th>
+                            <th>休憩時間</th>
+                            <th>勤務時間</th>
+                        </tr>
+                        @foreach($items as $item)
                             <tr>
-                                <a href="/attendance/{{$beforedate}}">前の日</a>
-                                <td>{{date('Y-m-d', strtotime($date))}}</td>
-                                <a href="/attendance/{{$afterdate}}">次の日</a>
+                                <td>{{$item->name}}</td>
+                                <td>{{substr($item->start_time, 11, 8)}}</td>
+                                <td>{{substr($item->end_time, 11, 8)}}</td>
+                                <td>{{$item->sum_resting_time}}</td>
+                                <td>{{$item->working_time}}</td>
                             </tr>
-                            <tr>
-                                <th>名前</th>
-                                <th>勤務開始</th>
-                                <th>勤務終了</th>
-                                <th>休憩時間</th>
-                                <th>勤務時間</th>
-                            </tr>
-                            @foreach($items as $item)
-                                <tr>
-                                    <td>{{$item->name}}</td>
-                                    <td>{{substr($item->start_time, 11, 8)}}</td>
-                                    <td>{{substr($item->end_time, 11, 8)}}</td>
-                                    <td>{{$item->sum_resting_time}}</td>
-                                    <td>{{$item->working_time}}</td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    </form>
+                        @endforeach
+                    </table>
                 </div>
             <!-- </div> -->
         </div>
