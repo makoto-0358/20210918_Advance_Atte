725904a59d413e798ae8a369d2809bba70aa9242
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-10-27 11:25:26 +0900

    ログアウト時のlayout関係性変更

---
+++ resources/views/layouts/navigationlogouted.blade.php
@@ -0,0 +1,13 @@
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Atte -->
+                <div class="flex-shrink-0 flex items-center">
+                    <p>Atte</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
