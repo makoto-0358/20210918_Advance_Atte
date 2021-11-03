fdfacc3b7148496800b0683efcc3a57e3dea1947
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-04 07:19:17 +0900

    不要な記述(誤記)削除

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,8 +1,4 @@
 <x-appmember-layout>
-    <div>
-        {{ Auth::user()->name }}さんお疲れ様です！
-    </div>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <!-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"> -->
