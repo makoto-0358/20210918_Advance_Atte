1da4546de560cf4dd969462b3dc509ef5bbfa307
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-07 16:30:48 +0900

    footer フォント等修正

--- resources/views/layouts/navigationfoot.blade.php
+++ resources/views/layouts/navigationfoot.blade.php
@@ -1,11 +1,11 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-center h-16">
             <div class="flex">
                 <!-- Atte -->
                 <div class="flex-shrink-0 flex items-center">
-                    <p>Atte, inc.</p>
+                    <p class="font-bold">Atte, inc.</p>
                 </div>
             </div>
         </div>
