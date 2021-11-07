ed226f18b385821b062b54bc12253c35cf101de8
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-08 07:53:23 +0900

    会員登録＆ログインページ UI修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,14 +17,14 @@
             <div>
                 <x-label for="email" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" placeholder="メールアドレス" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full bg-gray-100 border-4" type="email" placeholder="メールアドレス" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="block mt-1 w-full bg-gray-100 border-4"
                                 type="password"
                                 placeholder="パスワード"
                                 name="password"
@@ -32,23 +32,23 @@
             </div>
 
             <div class="flex items-center justify-center mt-4">
-                <table>
+                <table class="w-full">
                     <tr>
                         <td class="flex justify-center">
-                            <x-button class="ml-3">
+                            <x-button class="w-full flex justify-center bg-blue-700 text-base">
                                 {{ __('Log in') }}
                             </x-button>
                         </td>
                     </tr>
                     <tr>
-                        <td>
+                        <td class="w-full flex justify-center mt-10 text-sm text-gray-400">
                             アカウントをお持ちでない方はこちらから
                         </td>
                     </tr>
                     @if (Route::has('password.request'))
                     <tr>
                         <td>
-                            <a class="flex justify-center text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                            <a class="flex justify-center text-sm text-blue-700"  href="{{ route('register') }}">
                                 {{ __('Register') }}
                             </a>
                         </td>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,21 +14,21 @@
             <div>
                 <x-label for="name" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" placeholder="名前" name="name" :value="old('name')" required autofocus />
+                <x-input id="name" class="block mt-1 w-full bg-gray-100 border-4" type="text" placeholder="名前" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" placeholder="メールアドレス" name="email" :value="old('email')" required />
+                <x-input id="email" class="block mt-1 w-full bg-gray-100 border-4" type="email" placeholder="メールアドレス" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="block mt-1 w-full bg-gray-100 border-4"
                                 type="password"
                                 placeholder="パスワード"
                                 name="password"
@@ -39,29 +39,29 @@
             <div class="mt-4">
                 <x-label for="password_confirmation" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
+                <x-input id="password_confirmation" class="block mt-1 w-full bg-gray-100 border-4"
                                 type="password"
                                 placeholder="確認用パスワード"
                                 name="password_confirmation" required />
             </div>
 
             <div class="flex items-center justify-center mt-4">
-                <table>
+                <table class="w-full">
                     <tr>
                         <td class="flex justify-center">
-                            <x-button class="ml-4">
+                            <x-button class="w-full flex justify-center bg-blue-700 text-base">
                                 {{ __('Register') }}
                             </x-button>
                         </td>
                     </tr>
                     <tr>
-                        <td>
+                        <td class="w-full flex justify-center mt-10 text-sm text-gray-400">
                             アカウントをお持ちの方はこちらから
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <a class="flex justify-center text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                            <a class="flex justify-center text-sm text-blue-700" href="{{ route('login') }}">
                                 {{ __('Already registered?') }}
                             </a>
                         </td>
