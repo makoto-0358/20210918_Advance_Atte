a21039d9d1dc76158f82968bd4eed4f057aa5e2b
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-12 15:38:27 +0900

    日付別勤怠ページUIほぼ完成

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,34 +1,49 @@
 <x-appmember-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <!-- <div class="py-12"> -->
+        <!-- <div class="w-4/5 mx-auto flex content-between"> -->
             <!-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"> -->
-                <div class="p-6 border-t border-gray-200">
-                    <table>
-                        <tr>
-                            <a href="/attendance/{{$beforedate}}">前の日</a>
-                            <td>{{date('Y-m-d', strtotime($date))}}</td>
-                            <a href="/attendance/{{$afterdate}}">次の日</a>
+                <!-- <div> -->
+                    <table class="w-4/5 mx-auto flex justify-center">
+                        <tr class="flex justify-center items-center my-12">
+                            <td>
+                                <a href="/attendance/{{$beforedate}}">
+                                    <div class="mx-8 w-12 h-8 bg-white border-2 rounded border-solid border-blue-400 relatve">
+                                        <div class="absolute mt-3 ml-2 w-6 h-0 box-border border-t-2 border-solid border-blue-400 transform origin-left -rotate-12"></div>
+                                        <div class="absolute mt-3 ml-2 w-6 h-0 box-border border-b-2 border-solid border-blue-400 transform origin-left rotate-12"></div>
+                                    </div>
+                                </a>
+                            </td>
+                            <td class="text-2xl">{{date('Y-m-d', strtotime($date))}}</td>
+                            <td>
+                                <a href="/attendance/{{$afterdate}}">
+                                    <div class="mx-8 w-12 h-8 bg-white border-2 rounded border-solid border-blue-400 flex items-center">
+                                        <div class="ml-3 w-4 h-4 border-t-2 border-r-2 border-solid border-blue-400 transform rotate-45"></div>
+                                    </div>
+                                </a>
+                            </td>
                         </tr>
-                        <tr>
-                            <th>名前</th>
-                            <th>勤務開始</th>
-                            <th>勤務終了</th>
-                            <th>休憩時間</th>
-                            <th>勤務時間</th>
+                        <tr class="flex content-between border-t-2 border-gray-400">
+                            <th class="mx-20 my-5">名前</th>
+                            <th class="mx-20 my-5">勤務開始</th>
+                            <th class="mx-20 my-5">勤務終了</th>
+                            <th class="mx-20 my-5">休憩時間</th>
+                            <th class="mx-20 my-5">勤務時間</th>
                         </tr>
                         @foreach($items as $item)
-                            <tr>
-                                <td>{{$item->name}}</td>
-                                <td>{{substr($item->start_time, 11, 8)}}</td>
-                                <td>{{substr($item->end_time, 11, 8)}}</td>
-                                <td>{{$item->sum_resting_time}}</td>
-                                <td>{{$item->working_time}}</td>
+                            <tr class="flex content-between border-t-2 border-gray-400">
+                                <td class="mx-20 my-5">{{$item->name}}</td>
+                                <td class="mx-20 my-5">{{substr($item->start_time, 11, 8)}}</td>
+                                <td class="mx-20 my-5">{{substr($item->end_time, 11, 8)}}</td>
+                                <td class="mx-20 my-5">{{$item->sum_resting_time}}</td>
+                                <td class="mx-20 my-5">{{$item->working_time}}</td>
                             </tr>
                         @endforeach
                     </table>
-                </div>
+                <!-- </div> -->
             <!-- </div> -->
-        </div>
+        <!-- </div> -->
+    <!-- </div> -->
+    <div class="flex justify-center">
+        {{$items->links()}}
     </div>
-{{$items->links()}}
 </x-appmember-layout>
