58441321a9a8b6389fd5cfdae033985c7e42e53e
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-14 17:53:13 +0900

    table表記廃止

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,77 +23,77 @@
     <div class="py-12 flex flex-wrap mx-40 text-2xl">
         <div class="w-5/12 mx-auto my-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="py-20 px-50 bg-white border-b border-gray-200">
-                    <table class="flex item-center justify-center">
+                <div class="py-20 px-50 bg-white border-b border-gray-200 flex item-center justify-center">
+                    <!-- <table class="flex item-center justify-center"> -->
                         @isset($attendance)
-                            <tr><td class="font-bold text-gray-400">勤務開始</td></tr>
+                            <div class="font-bold text-gray-400">勤務開始</div>
                         @else
                         <form action="/attendance/start" method="post">
                             @csrf
-                            <tr><td><button class="font-bold" type="submit">勤務開始</button></td></tr>
+                            <button class="font-bold" type="submit">勤務開始</button>
                         </form>
                         @endisset
-                    </table>
+                    <!-- </table> -->
                 </div>
             </div>
         </div>
         <div class="w-5/12 mx-auto my-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="py-20 px-50 bg-white border-b border-gray-200">
-                    <table class="flex item-center justify-center">
+                <div class="py-20 px-50 bg-white border-b border-gray-200 flex item-center justify-center">
+                    <!-- <table class="flex item-center justify-center"> -->
                         @isset($attendance)
                             @empty($rest)
                             <form action="/attendance/end" method="post">
                                 @csrf
-                                <tr><td><button class="font-bold" type="submit">勤務終了</button></td></tr>
+                                <button class="font-bold" type="submit">勤務終了</button>
                             </form>
                             @else
-                            <tr><td class="font-bold text-gray-400">勤務終了</td></tr>
+                            <div class="font-bold text-gray-400">勤務終了</div>
                             @endempty
                         @else
-                        <tr><td class="font-bold text-gray-400">勤務終了</td></tr>
+                        <div class="font-bold text-gray-400">勤務終了</div>
                         @endisset
-                    </table>
+                    <!-- </table> -->
                 </div>
             </div>
         </div>
         <div class="w-5/12 mx-auto my-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="py-20 px-50 bg-white border-b border-gray-200">
-                    <table class="flex item-center justify-center">
+                <div class="py-20 px-50 bg-white border-b border-gray-200 flex item-center justify-center">
+                    <!-- <table class="flex item-center justify-center"> -->
                         @isset($attendance)
                             @empty($rest)
                             <form action="/rest/start" method="post">
                                 @csrf
-                                <tr><td><button class="font-bold" type="submit">休憩開始</button></td></tr>
+                                <button class="font-bold" type="submit">休憩開始</button>
                             </form>
                             @else
-                            <tr><td class="font-bold text-gray-400">休憩開始</td></tr>
+                            <div class="font-bold text-gray-400">休憩開始</div>
                             @endempty
                         @else
-                        <tr><td class="font-bold text-gray-400">休憩開始</td></tr>
+                        <div class="font-bold text-gray-400">休憩開始</div>
                         @endisset
-                    </table>
+                    <!-- </table> -->
                 </div>
             </div>
         </div>
         <div class="w-5/12 mx-auto my-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="py-20 px-50 bg-white border-b border-gray-200">
-                    <table class="flex item-center justify-center">
+                <div class="py-20 px-50 bg-white border-b border-gray-200 flex item-center justify-center">
+                    <!-- <table class="flex item-center justify-center"> -->
                         @isset($attendance)
                             @isset($rest)
                             <form action="/rest/end" method="post">
                                 @csrf
-                                <tr><td><button class="font-bold" type="submit">休憩終了</button></td></tr>
+                                <button class="font-bold" type="submit">休憩終了</button>
                             </form>
                             @else
-                            <tr><td class="font-bold text-gray-400">休憩終了</td></tr>
+                            <div class="font-bold text-gray-400">休憩終了</div>
                             @endisset
                         @else
-                        <tr><td class="font-bold text-gray-400">休憩終了</td></tr>
+                        <div class="font-bold text-gray-400">休憩終了</div>
                         @endisset
-                    </table>
+                    <!-- </table> -->
                 </div>
             </div>
         </div>
