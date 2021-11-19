28f94f37836e0a9f9a51795f21ed1c3698c3c084
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-19 11:54:38 +0900

    テーブルtdの高さ設定をパディングへ変更

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -22,19 +22,19 @@
                     <!-- </div> -->
                     <table class="ml-8 flex md:justify-center overflow-x-auto whitespace-no-wrap md:w-4/5 md:mx-auto">
                         <tr class="flex flex-no-wrap border-t-2 border-gray-400">
-                            <th class="w-24 h-12 flex justify-center items-center md:w-56">名前</th>
-                            <th class="w-24 h-12 flex justify-center items-center md:w-56">勤務開始</th>
-                            <th class="w-24 h-12 flex justify-center items-center md:w-56">勤務終了</th>
-                            <th class="w-24 h-12 flex justify-center items-center md:w-56">休憩時間</th>
-                            <th class="w-24 h-12 flex justify-center items-center md:w-56">勤務時間</th>
+                            <th class="w-24 py-4 flex justify-center items-center md:w-56">名前</th>
+                            <th class="w-24 py-4 flex justify-center items-center md:w-56">勤務開始</th>
+                            <th class="w-24 py-4 flex justify-center items-center md:w-56">勤務終了</th>
+                            <th class="w-24 py-4 flex justify-center items-center md:w-56">休憩時間</th>
+                            <th class="w-24 py-4 flex justify-center items-center md:w-56">勤務時間</th>
                         </tr>
                         @foreach($items as $item)
                             <tr class="flex content-between border-t-2 border-gray-400">
-                                <td class="w-24 min-h-100% flex justify-center items-center md:w-56">{{$item->name}}</td>
-                                <td class="w-24 min-h-100% flex justify-center items-center md:w-56">{{substr($item->start_time, 11, 8)}}</td>
-                                <td class="w-24 min-h-100% flex justify-center items-center md:w-56">{{substr($item->end_time, 11, 8)}}</td>
-                                <td class="w-24 min-h-100% flex justify-center items-center md:w-56">{{$item->sum_resting_time}}</td>
-                                <td class="w-24 min-h-100% flex justify-center items-center md:w-56">{{$item->working_time}}</td>
+                                <td class="w-24 py-4 flex justify-center items-center md:w-56">{{$item->name}}</td>
+                                <td class="w-24 py-4 flex justify-center items-center md:w-56">{{substr($item->start_time, 11, 8)}}</td>
+                                <td class="w-24 py-4 flex justify-center items-center md:w-56">{{substr($item->end_time, 11, 8)}}</td>
+                                <td class="w-24 py-4 flex justify-center items-center md:w-56">{{$item->sum_resting_time}}</td>
+                                <td class="w-24 py-4 flex justify-center items-center md:w-56">{{$item->working_time}}</td>
                             </tr>
                         @endforeach
                     </table>
