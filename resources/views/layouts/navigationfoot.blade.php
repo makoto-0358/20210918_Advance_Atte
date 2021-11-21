d7400fe30f13ea71943149822b7e54b72cecd574
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-21 14:56:57 +0900

    コード整理

--- resources/views/layouts/navigationfoot.blade.php
+++ resources/views/layouts/navigationfoot.blade.php
@@ -1,13 +1,1 @@
-<nav class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4">
-        <div class="flex justify-center h-16">
-            <div class="flex">
-                <!-- Atte, inc. -->
-                <div class="flex items-center">
-                    <p class="font-bold">Atte, inc.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
+<div class="w-full h-16 mx-auto bg-white flex justify-center items-center font-bold">Atte, inc.</div>
