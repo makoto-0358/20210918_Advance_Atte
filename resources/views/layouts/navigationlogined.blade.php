fa63d3c4108eb31457cbd689daa790b6903a0507
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-11-21 14:35:30 +0900

    コード整理

--- resources/views/layouts/navigationlogined.blade.php
+++ resources/views/layouts/navigationlogined.blade.php
@@ -1,29 +1,23 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4">
         <div class="flex justify-center md:justify-between py-0 flex-col md:flex-row">
-            <!-- <div class="flex"> -->
-                <!-- Atte -->
-                <div class="flex items-center justify-center text-3xl font-bold">Atte
-                    <!-- <p class="text-3xl font-bold">Atte</p> -->
-                </div>
-            <!-- </div> -->
+            <!-- Atte -->
+            <div class="flex items-center justify-center text-3xl font-bold">Atte</div>
 
-            <!-- <div class="flex"> -->
-                <!-- Links -->
-                <div class="flex-shrink-0 flex items-center justify-center">
-                    <a class="m-2 font-bold md:m-6" href="{{route('index')}}">ホーム</a>
-                    <a class="m-2 font-bold md:m-6" href="{{route('attendance')}}">日付一覧</a>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <a class="m-2 font-bold md:m-6" href="route('logout')"
-                            onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                        </a>
-                    </form>
-                </div>
-            <!-- </div> -->
+            <!-- Links -->
+            <div class="flex-shrink-0 flex items-center justify-center">
+                <a class="m-2 font-bold md:m-6" href="{{route('index')}}">ホーム</a>
+                <a class="m-2 font-bold md:m-6" href="{{route('attendance')}}">日付一覧</a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a class="m-2 font-bold md:m-6" href="route('logout')"
+                        onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                    </a>
+                </form>
+            </div>
         </div>
     </div>
 </nav>
