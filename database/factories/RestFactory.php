40b0d5fd9ec751d96a4ce29cfec2ab121db2eabc
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-12-28 13:16:59 +0900

    RestFactory一旦完成

--- database/factories/RestFactory.php
+++ database/factories/RestFactory.php
@@ -21,8 +21,11 @@ class RestFactory extends Factory
      */
     public function definition()
     {
+        $time = $this->faker->dateTimeBetween($dstartTime='+1hour', $endDate='3hour');
         return [
-            //
+            'attendance_id' => Attendancer::factory(),
+            'start_time' => $time->format('H:i:s'),
+            'end_time' => $time->modify($dstartTime='+1hour', $endDate='+2hour')->format('H:i:s')
         ];
     }
 }
