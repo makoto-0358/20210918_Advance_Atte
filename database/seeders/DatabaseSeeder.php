d055184aeb2e8353e74ba0f163102a53cd6e429a
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-12-28 13:28:00 +0900

    DatabaseSeeder修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Attendance::factory(10)->create();
-        \App\Models\Rest::factory(10)->create();
+        \App\Models\Attendance::factory(20)->create();
+        \App\Models\Rest::factory(40)->create();
     }
 }
