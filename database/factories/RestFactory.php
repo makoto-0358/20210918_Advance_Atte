53f198e3c9fc806541de70d610ec84bb5c08974b
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-12-31 19:28:33 +0900

    過剰なコード削除

--- database/factories/RestFactory.php
+++ database/factories/RestFactory.php
@@ -22,33 +22,14 @@ class RestFactory extends Factory
      */
     public function definition()
     {
-        // $rest = Attendance::factory()->start_time->modify('+1hour');
-        // $attendance_id = Attendance::factory();
-        // $rest = Attendance::find($attendance_id)->start_time;
-        // $attendance_id = Attendance::factory()->id()->get();
-        // $rest = Rest::wherenull('end_time')->first();
         $attendance = Attendance::wherenotnull('start_time')->latest('id')->first();
         $attendance_id = $attendance['id'];
         $rest = Attendance::find($attendance_id);
         $rest = $rest['start_time'];
 
         return [
-            // 'attendance_id' => Attendance::factory(),
-            // 'start_time' => function(array $attributes){
-            //     return Attendance::find($attributes[$attendance_id])->start_time->modify('+1hour')->format('Y-m-d H:i:s');
-            // },
-            // 'end_time' => function(array $attributes){
-            //     return Attendance::find($attributes[$attendance_id])->start_time->modify('+3hour')->format('Y-m-d H:i:s');
-            // },
-            // 'start_time' => Attendance::find($attendance_id)->start_time->modify('+1hour')->format('Y-m-d H:i:s'),
-            // 'end_time' => Attendance::find($attendance_id)->start_time->modify('+3hour')->format('Y-m-d H:i:s')
-
-            // 'start_time' => Attendance::find($rest['attendance_id'])->start_time->modify('+1hour')->format('Y-m-d H:i:s'),
-            // 'end_time' => Attendance::find($rest['attendance_id'])->start_time->modify('+3hour')->format('Y-m-d H:i:s'),
-
             'start_time' => date('Y-m-d H:i:s', strtotime("$rest +1 hour")),
             'end_time' => date('Y-m-d H:i:s', strtotime("$rest +3 hour")),
-
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,24 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory()->count(2)->create()->each(function($user){
-        //     Attendance::factory()->count(2)->make()->each(function($attendance)use($user){
-        //         $user->attendances()->save($attendance);
-        //     });
-        // });
-
-        // Rest::factory()->count(1)->make()->each(function($rest)use($attendance){
-        //     $attendance->rests()->save($rest);
-        // });
-
-        // User::factory()->count(2)->create();
-
-        // $user = User::factory()->create();
-        // $attendance = Attendance::factory()->count(2)->for($user)->create();
-        // $rest = Rest::factory()->count(2)->for($attendance)->create();
-
-        // $rests = Rest::factory()->count(2)->for($attendance)->create();
-
         User::factory()->count(2)->create()->each(function($user){
             Attendance::factory()->count(2)->make()->each(function($attendance)use($user){
                 $user->attendances()->save($attendance);
@@ -42,17 +24,5 @@ class DatabaseSeeder extends Seeder
                 });
             });
         });
-
-        // User::factory()->count(2)->create()->each(function($user){
-        //     Attendance::factory()->count(2)->make()->each(function($attendance)use($user){
-        //         $user->attendances()->save($attendance);
-        //     });
-        // })->each(function($attendance){
-        //     Rest::factory()->count(2)->make()->each(function($rest)use($attendance){
-        //         $attendance->rests()->save($rest);
-        //     });
-        // });
-
-
     }
 }
