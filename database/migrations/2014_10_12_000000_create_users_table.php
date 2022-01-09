2f913a2d24f89143045a843a6a246b72b7f966d2
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2022-01-09 13:06:52 +0900

    table 修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ class CreateUsersTable extends Migration
             $table->unsignedBiginteger('id')->autoIncrement();
             $table->string('name', 100);
             $table->string('email', 255)->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 100);
+            $table->rememberToken();
             $table->timestamp('created_at')->useCurrent()->nullable();
             $table->timestamp('updated_at')->useCurrent()->nullable();
         });
