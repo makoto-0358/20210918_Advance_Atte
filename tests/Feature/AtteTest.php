145a7647cc81b620748976c54dd5da4a50656e42
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2022-01-24 16:15:26 +0900

    ユニットテスト実装

--- tests/Feature/AtteTest.php
+++ tests/Feature/AtteTest.php
@@ -77,57 +77,34 @@ class AtteTest extends TestCase
 
         $this->assertGuest();
     }
+
+    //  /ログイン中にpostへ行けることの確認
+    // public function test_post()
+    // {
+    //     $user = User::factory()->create();
+    //     $response = $this->actingAs($user);
+
+    //     $response = $this->post('/rest/start');
+    //     $response->assertRedirect(RouteServiceProvider::HOME);
+    // }
     
     // 勤務中でない場合、勤務開始できることの確認。
     public function test_attendance_start()
     {
         $user = User::factory()->create();
-        $responce = $this->actingAs($user);
+        $response = $this->actingAs($user);
         $attendance = '';
+        $dt1 = now()->addHour();
         $attendance = [
             'user_id' => $user->id,
-            'start_time' => now(),
+            'start_time' => $dt1,
         ];
-        $responce = $this->post('/attendance/start', $attendance);
-
-        $responce = $this->assertDatabaseHas('attendances', [
+        Attendance::create($attendance);
+        $response = $this->post('/attendance/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('attendances', [
             'user_id' => $user->id,
-            'start_time' => now(),
-        ]);
-    }
-
-    // 勤務中の場合、勤務開始できないことの確認。
-    public function test_attendance_notstart()
-    {
-        $user = User::factory()->create();
-        $responce = $this->actingAs($user);
-
-        $attendance = '';
-
-        $dt = now();
-        $attendance = [
-            'user_id' => $user->id,
-            'start_time' => $dt,
-            'end_time' => null,
-
-        ];
-        $responce = $this->post('/attendance/start', $attendance);
-        $responce = $this->assertDatabaseHas('attendances', [
-            'user_id' => $user->id,
-            'start_time' => $dt,
-            'end_time' => null,
-        ]);
-
-        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->whereNull('end_time')->first();
-        $dt->addHours(2);
-        $attendance = [
-            'user_id' => $user->id,
-            'start_time' => $dt,
-        ];
-        $responce = $this->post('/attendance/start', $attendance);
-        $responce = $this->assertDatabaseMissing('attendances', [
-            'user_id' => $user->id,
-            'start_time' => $dt,
+            'start_time' => $dt1,
         ]);
     }
 
@@ -135,19 +112,20 @@ class AtteTest extends TestCase
     public function test_attendance_end()
     {
         $user = User::factory()->create();
-        $responce = $this->actingAs($user);
+        $response = $this->actingAs($user);
 
         $attendance = '';
-        
-        $dt1 = now();
+        $dt1 = now()->addHour();
         $attendance = [
             'user_id' => $user->id,
             'start_time' => $dt1,
             'end_time' => null,
 
         ];
-        $responce = $this->post('/attendance/start', $attendance);
-        $responce = $this->assertDatabaseHas('attendances', [
+        Attendance::create($attendance);
+        $response = $this->post('/attendance/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('attendances', [
             'user_id' => $user->id,
             'start_time' => $dt1,
             'end_time' => null,
@@ -159,7 +137,9 @@ class AtteTest extends TestCase
         $form['end_time'] = $dt2;
         unset($form["_token"]);
         $attendance->update($form);
-        $responce = $this->assertDatabaseHas('attendances', [
+        $response = $this->post('/attendance/end');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('attendances', [
             'user_id' => $user->id,
             'start_time' => $dt1,
             'end_time' => $dt2,
@@ -170,48 +150,93 @@ class AtteTest extends TestCase
     public function test_rest_start()
     {
         $user = User::factory()->create();
-        $responce = $this->actingAs($user);
+        $response = $this->actingAs($user);
 
-        $attenance = '';
-        // $dt1 = now();
-        // $time = Attendance::factory()->faker->dateTimeBetween($startTime='-1hour', $endDate='+1hour');
-        // $dt1 = $time->format('Y-m-d H:i:s');
-        $dt1 = Attendance::factory()->start_time;
-        dd($user);
-        // $attendance = Attendance::factory()->create();
-        // dd($time);
-        // dd($user);
-        // dd($attendance);
-        // $dt1 = $time['start_time'];
-        // dd($dt1);
+        $attendance = '';
+        $dt1 = now()->addHour();
         $attendance = [
             'user_id' => $user->id,
             'start_time' => $dt1,
             'end_time' => null,
         ];
-        $responce = $this->post('/attendance/start', $attendance);
-        $responce = $this->assertDatabaseHas('attendances', [
+        Attendance::create($attendance);
+        $response = $this->post('/attendance/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('attendances', [
             'user_id' => $user->id,
             'start_time' => $dt1,
             'end_time' => null,
         ]);
         
-        // $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->whereNull('end_time')->first();
-        // $dt2 = now()->addHours(2);
-        // $rest = '';
-        // $rest = [
-        //     'attendance_id' => $attendance->id,
-        //     'start_time' => $dt2,
-        //     'end_time' => null,
-        // ];
-        // // dd($rest['start_time']);
-        // Rest::create($rest);
-        // // $responce = $this->post('/rest/start', $rest);
-        // dd($rest['start_time']);
-        // $responce = $this->assertDatabaseHas('rests', [
-        //     'attendance_id' => $attendance->id,
-        //     'start_time' => $dt2,
-        //     'end_time' => null,
-        // ]);
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->whereNull('end_time')->first();
+        $dt2 = now()->addHours(2);
+        $rest = '';
+        $rest = [
+            'attendance_id' => $attendance->id,
+            'start_time' => $dt2,
+            'end_time' => null,
+        ];
+        Rest::create($rest);
+        $response = $this->post('/rest/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('rests', [
+            'attendance_id' => $attendance->id,
+            'start_time' => $dt2,
+            'end_time' => null,
+        ]);
+    }
+
+    // 勤務中の場合、休憩中であれば休憩終了できることの確認。
+    public function test_rest_end()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user);
+
+        $attendance = '';
+        $dt1 = now()->addHour();
+        $attendance = [
+            'user_id' => $user->id,
+            'start_time' => $dt1,
+            'end_time' => null,
+        ];
+        Attendance::create($attendance);
+        $response = $this->post('/attendance/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('attendances', [
+            'user_id' => $user->id,
+            'start_time' => $dt1,
+            'end_time' => null,
+        ]);
+        
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->whereNull('end_time')->first();
+        $dt2 = now()->addHours(2);
+        $rest = '';
+        $rest = [
+            'attendance_id' => $attendance->id,
+            'start_time' => $dt2,
+            'end_time' => null,
+        ];
+        Rest::create($rest);
+        $response = $this->post('/rest/start');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('rests', [
+            'attendance_id' => $attendance->id,
+            'start_time' => $dt2,
+            'end_time' => null,
+        ]);
+
+        $attendance = Attendance::where('user_id', Auth::user()->id)->latest('id')->whereNull('end_time')->first();
+        $rest = Rest::where('attendance_id', $attendance['id'])->latest('id')->whereNull('end_time')->first();
+        $dt3 = now()->addHours(3);
+        $form['end_time'] = $dt3;
+        unset($form["_token"]);
+        $rest->update($form);
+        $response = $this->post('/rest/end');
+        $response->assertRedirect(route('index'));
+        $response = $this->assertDatabaseHas('rests', [
+            'attendance_id' => $attendance->id,
+            'start_time' => $dt2,
+            'end_time' => $dt3,
+        ]);
     }
 }
